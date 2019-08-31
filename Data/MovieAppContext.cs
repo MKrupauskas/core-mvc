ae4665993ad4ccf5037fa7c64610e76efe540537
Mykolas Krupauskas <mkrupauskas@gmail.com>
2019-08-31 13:37:00 +0200

    add files for core-mvc project

---
+++ Data/MovieAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Models
+{
+    public class MovieAppContext : DbContext
+    {
+        public MovieAppContext (DbContextOptions<MovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApp.Models.Movie> Movie { get; set; }
+    }
+}
